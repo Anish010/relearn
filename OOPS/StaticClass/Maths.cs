be4c679fa6d2b2c1be2582edb36c1ef88ed661e7
anish-tezo-104 <anish.m@Technovert.com>
2024-07-16 21:58:22 +0530

    added oops

---
+++ OOPS/StaticClass/Maths.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public static class Maths
+    {
+        private static readonly double Pi;
+
+
+        // static class can have only static non parameterized constructor
+        static Maths()
+        {
+            Pi = 3.141592653589793;
+        }
+
+        // menthods in static class need to be static. Static class cannot create instance members
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return (double)a / b;
+        }
+
+        public static double CalculateCircleArea(double radius)
+        {
+            return Pi * radius * radius;
+        }
+    }
+}
---
+++ OOPS/StaticClass/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StaticClass;
+public class Mains
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Addition: " + Maths.Add(5, 3));
+        Console.WriteLine("Subtraction: " + Maths.Subtract(5, 3));
+        Console.WriteLine("Multiplication: " + Maths.Multiply(5, 3));
+        Console.WriteLine("Division: " + Maths.Divide(5, 3));
+        Console.WriteLine("Circle Area: " + Maths.CalculateCircleArea(5));
+    }
+}
